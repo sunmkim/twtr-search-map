a2c1ef3487f1747d7a579aff7fdb486496e24d75
Sun Kim <sunkim@Ralphs-iMac.local>
2014-11-14 18:04:06 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# all the imports
+from flask import Flask, request, session, g, redirect, url_for, \
+     abort, render_template, flash
+
+# create application
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+	return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
