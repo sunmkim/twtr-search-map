8773252177acc6930bd3ff9171a17a0a4d8e2b55
Sun Kim <sunkim924@gmail.com>
2014-11-17 00:59:28 -0500

    updated app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 #!venv/bin/python
 
 # all the imports
-from flask import Flask, request, session, g, redirect, url_for, \
+from flask import Flask, request, session, redirect, url_for, \
      abort, render_template, flash
+
+import tweepy
 
 # create application
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/main', methods=['GET','POST'])
 def main():
-	return render_template('index.html')
+  if request.method == 'GET':
+    return render_template('index.html')
+  else:
+    tweet = request.form['tweet']
+    print tweet
+    return 'OK'
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/google_map.js
+++ static/google_map.js
@@ -1,10 +1,10 @@
 function initialize() {
     var mapOptions = {
         center: {
-            lat: -34.397,
-            lng: 150.644
+            lat: 40.713,
+            lng: -74.000
         },
-        zoom: 3
+        zoom: 11
     };
     var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
