ae91bad19b1e13afca019620710daeb936c27432
Sun Kim <sunkim@Ralphs-iMac.local>
2014-11-19 16:24:49 -0500

    finished real-time streaming to client

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 #!venv/bin/python
 
-# all the imports
-from flask import Flask, request, url_for, render_template
+from flask import Flask, request, render_template, Response, stream_with_context, redirect, url_for
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
-import tweepy
-from tstream import StdOutListener
 
-# create application
-app = Flask(__name__)
+import tweepy, redis
+from tstream import StdOutListener
 
 # consumer key and secrets
 consumer_key = CONSUMER_KEY
@@ -22,6 +19,10 @@ access_token_secret = ACCESS_TOKEN_SECRET
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 
+# create application
+app = Flask(__name__)
+red = redis.StrictRedis()
+
 
 @app.route('/')
 def index():
@@ -29,17 +30,35 @@ def index():
 
 
 @app.route('/search', methods=['POST'])
-def search():
+# gets search-keyword and starts stream
+def streamTweets():
 	search_term = request.form['tweet']
 	search_term_hashtag = '#' + search_term
-	
-	listener = StdOutListener()
-	
+	# instantiate listener
+	listener = StdOutListener()	
+	# stream object uses listener we instantiated above to listen for data
 	stream = tweepy.Stream(auth, listener)
 	stream.filter(track=[search_term or search_term_hashtag],
 		async=True) # make sure stream is non-blocking
-	return '200 OK'
+	redirect('/stream') # execute '/stream' sse
+	return render_template('index.html')
+
+
+@app.route('/stream')
+def stream():
+	# we will use Pub/Sub process to send real-time tweets to client
+	def event_stream():
+		# instantiate pubsub
+		pubsub = red.pubsub()
+		# subscribe to tweet_stream channel
+		pubsub.subscribe('tweet_stream')
+		for message in pubsub.listen():
+			yield 'data: %s\n\n' % message['data']
+	return Response(stream_with_context(event_stream()), mimetype="text/event-stream")
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug = True, port=8000)
+
+
+
--- tstream.py
+++ tstream.py
@@ -1,15 +1,20 @@
 #!venv/bin/python
 
 # all the imports
-from flask import Flask
+from flask import Flask, request
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
 
-import tweepy
+import tweepy, redis
 import simplejson as json
+
+red = redis.StrictRedis()
+
 
 # listener that handles streaming data
 class StdOutListener(tweepy.StreamListener):
+	
 	def on_connect(self):
-		print 'Connection established...'
+		print 'Stream starting...'
 
 	# method called when raw data is received from connection
 	def on_data(self, data):
@@ -17,13 +22,15 @@ class StdOutListener(tweepy.StreamListener):
 		# listen only for tweets that is geo-location enabled
 		if decoded['geo']:
 			tweet = {}
-			tweet['screen_name'] = decoded['user']['screen_name']
+			tweet['screen_name'] = '@'+decoded['user']['screen_name']
 			tweet['text'] = decoded['text'].encode('ascii', 'ignore')
 			tweet['coord'] = decoded['geo']['coordinates']
 			tweet['created_at'] = decoded['created_at']
-			print tweet
-			print ''
-		return True
+			print 'A tweet received'
+			# publish to 'tweet_stream' channel
+			red.publish('tweet_stream', json.dumps(tweet))
+			return True
 
 	def on_error(self, status):
 		print status
+
